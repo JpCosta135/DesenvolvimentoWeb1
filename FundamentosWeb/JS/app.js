2e2b7489c01ca0cef0f7e079f54231701c5807a5
João Paulo <130411184+JpCosta135@users.noreply.github.com>
2026-09-15 17:39:12 -0300

    Add files via upload

--- FundamentosWeb/JS/app.js
+++ FundamentosWeb/JS/app.js
@@ -1,45 +1,43 @@
-const input = document.querySelector("#todo-input");
-const list = document.querySelector("#todo-list");
-const form = document.querySelector("#todo-form");
+const form = document.querySelector('#todo-form');
+const input = document.querySelector('#todo-input');
+const list = document.querySelector('#todo-list');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
-    if (input.value.trim() === "") {
-        return null;
-    } else {
-        const li = document.createElement("li");
-        li.classList.add("todo-item");
-        li.textContent = input.value;
-        const button = document.createElement("button");
-        button.textContent = "\u00D7";
-        button.classList.add("btn-remover");
-        li.appendChild(button);
-        button.addEventListener("click", (event) => {
-            li.remove();
-            atualizarContador();
-            event.stopPropagation();
-        });
-        list.appendChild(li);
-        input.value = "";
-        input.focus();
+    if (input.value.trim() == '') {
+        return null
+    }
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.textContent = input.value;
+    const button = document.createElement('button');
+    button.textContent = "\u00D7";
+    button.classList.add('delete-button');
+    li.appendChild(button);
+    button.addEventListener('click', function() {
+        li.remove();
+        atualizarContador();
+        event.stopPropagation();
+    
+    });
+    list.appendChild(li);
+    atualizarContador();
+    input.value = '';
+    input.focus();
+});
+
+list.addEventListener('click', function(event) {
+    console.log(event.target);
+    console.log(event.target.classList.contains('todo-item'));
+    if (event.target.classList.contains('todo-item')) {
+        event.target.classList.toggle('done');
         atualizarContador();
     }
 });
 
-list.addEventListener ("click", (event) => {
-
-    event.target;
-    event.target.classList.contains("todo-item");
-    if (event.target.classList.contains("todo-item")) {
-        event.target.classList.toggle("done");
-        atualizarContador();
-    }
-
-});
-
-
- function atualizarContador() {
-    const contador = document.querySelector("#contador");
-    let valor = document.querySelectorAll(".todo-item:not(.done)");
-    document.querySelector("#contador").textContent = valor.length;
+const contador = document.querySelector('#contador');
+function atualizarContador () {
+    const pendentes = list.querySelectorAll('.todo-item:not(.done)').length;
+    contador.textContent = `Tarefas pendentes: ${pendentes}`;
 }
+    
